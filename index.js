e99302a40766061d32d48219519d697804fcd825
hermes-4 <ebenezeroforia08@gmail.com>
2023-12-18 08:42:34 +0000

    Added a Patient registration endpoint

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ mongoose.connect('mongodb://localhost:27017/')
     console.log('Connected to MongoDB')}
 ).catch((error)=>console.log(error.message));
 
+app.post("/patient", async(req,res) => {
+    const check = await Patient.find(
+        {FirstName:req.body.firstName,Surname:req.body.surName},
+        {FirstName:true}
+    );
+  
+        if (check.length==0) {
+            const patient = await Patient.create(req.body);
+            res.status(200).json(patient);
+           }    
+           else {
+            res.status(404).json({message:"Patient's records is already present"}); 
+            console.log(check);
+           }
+
+})
+
+
+
+
+
+
 app.listen(3000, ()=> {
     console.log('Node server is running')
 });
